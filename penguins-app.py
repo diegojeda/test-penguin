c712bf68a3cb22583920e6ee344953413c595fd3
diegojeda <66706753+diegojeda@users.noreply.github.com>
2020-08-29 19:23:05 -0500

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 DATE_TIME = "date/time"
-DATA_URL = ('https://raw.githubusercontent.com/diegojeda/Bit-Optimization-ML/master/BLCat.csv?token=AP452QLHAGN3BO5MWGDC37C7JHAT6')
+DATA_URL = ('https://raw.githubusercontent.com/diegojeda/Bit-Optimization-ML/master/BLCat.csv?token=AP452QJD3G6RCLCERTJIZWS7JLYZM')
 
 st.title("Dashboard Para Visualizacion de Datos")
 st.markdown("""Esta aplicacion le permite visualizar los datos de performance de las brocas de su campo""")
